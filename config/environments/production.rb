e761255b0b0c22881bd6772a291e8bc5c76e2672
Dan Bunker <dan@Dans-MacBook-Pro.local>
2015-09-22 22:35:19 -0600

    mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,15 @@ Rails.application.configure do
   #     :exception_recipients => %w{info@totalguitarandbass.com}
   #   }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "gmail.com",
+        :user_name            => "mcirque@gmail.com",
+        :password             => "Tele11yak",
+        :authentication       => :login,
+        :enable_starttls_auto => true
+  }
+
 end
